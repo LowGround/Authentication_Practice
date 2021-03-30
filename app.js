6de8e5b7e0e08455cdadc55204369ca6cf5689a5
Edward Lee <jung0min101@gmail.com>
2021-03-30 15:33:47 -0700

    Cookies and Sessions Practice

--- app.js
+++ app.js
@@ -6,8 +6,13 @@ const ejs = require('ejs');
 const mongoose = require('mongoose');
 // const encrypt = require('mongoose-encryption');
 const md5 = require("md5");
-const bcrypt = require('bcrypt'); // adding salt rounds to hashed passwords using bcrypt
-const saltRounds = 10;
+// const bcrypt = require('bcrypt'); // adding salt rounds to hashed passwords using bcrypt
+// const saltRounds = 10;
+
+//Learning how to create a web browsing session + cookies
+const session = require('express-session');
+const passport = require('passport');
+const passportLocalMongoose = require("passport-local-mongoose");
 
 const app = express();
 
@@ -15,13 +20,31 @@ app.use(bodyParser.urlencoded({extended: true}));
 app.set('view engine', 'ejs');
 app.use(express.static('public'));
 
+app.use(session({
+    secret: "password",
+    resave: false,
+    saveUninitialized: false
+}));
+
+
+app.use(passport.initialize());
+app.use(passport.session());
+
+
+
+
+
+
 mongoose.connect("mongodb://localhost:27017/userDB", {useNewUrlParser: true})
+mongoose.set("useCreateIndex", true);
 
 
 const userSchema = new mongoose.Schema ({
     email: String,
     password: String
 });
+
+userSchema.plugin(passportLocalMongoose);
 
 
 
@@ -30,7 +53,9 @@ const userSchema = new mongoose.Schema ({
 
 const User = mongoose.model("User", userSchema);
 
-
+passport.use(User.createStrategy());
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
 
 app.get("/", function(req, res){
     res.render('home')
@@ -50,43 +75,43 @@ app.get("/register", function(req, res){
 
 
 app.post('/register', function(req, res){
-    bcrypt.hash(req.body.password, saltRounds, function(err, hash){
-        const newUser = new User({
-            email: req.body.username,
-            // password: md5(req.body.password) //use the md5 package to turn our password into a hash
-            password: hash // using bcrypt now to add salt rounds to the hash
-        })
-        newUser.save(function(err){
-            if (err) {
-                console.log(err);
-            } else {
-                res.render("secrets"); 
-            }
-        })
-    })
+    // bcrypt.hash(req.body.password, saltRounds, function(err, hash){
+    //     const newUser = new User({
+    //         email: req.body.username,
+    //         // password: md5(req.body.password) //use the md5 package to turn our password into a hash
+    //         password: hash // using bcrypt now to add salt rounds to the hash
+    //     })
+    //     newUser.save(function(err){
+    //         if (err) {
+    //             console.log(err);
+    //         } else {
+    //             res.render("secrets"); 
+    //         }
+    //     })
+    // })
     
 })
 
 
 app.post('/login', function(req,res){
-    const username = req.body.username;
-    // const password = md5(req.body.password);
-    const password = req.body.password;
+    // const username = req.body.username;
+    // // const password = md5(req.body.password);
+    // const password = req.body.password;
 
-    User.findOne({email: username}, function(err, foundUser){
-        if (err) {
-            console.log(err);
-        } else {
-            if (foundUser) {
-                 bcrypt.compare(password, foundUser.password, function(err, result){
-                    if (result === true) {
-                        res.render("secrets")        
-                    }
-                 })
+    // User.findOne({email: username}, function(err, foundUser){
+    //     if (err) {
+    //         console.log(err);
+    //     } else {
+    //         if (foundUser) {
+    //              bcrypt.compare(password, foundUser.password, function(err, result){
+    //                 if (result === true) {
+    //                     res.render("secrets")        
+    //                 }
+    //              })
                 
-                }
-            }
-    }); 
+    //             }
+    //         }
+    // }); 
 });
 
 
